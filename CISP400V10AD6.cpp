fb46210f4cd9ab27cedf0cc21115b8fd4afcd8ba
EdwinKam <64185146+EdwinKam@users.noreply.github.com>
2020-05-01 17:02:31 -0700

    Add files via upload

---
+++ CISP400V10AD6.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include"Date.h"
+#include"Employee.h"
+#include"Complex.h"
+#include"Stack.h"
+using namespace std;
+
+int main() {
+	cout << "Testing integers--------------";
+	cout << "creat 10 element integer stack";
+
+
+}
